2afd6896f9869c5377cd4a74fe92b41327641d81
jjgb2311 <jgamezb@miumg.edu.gt>
2019-11-04 23:08:08 -0600

    Piclab con Galeria

---
+++ Pic Lab 1.0/PicLab/top_menu.php
@@ -0,0 +1,28 @@
+    <!-- Fixed navbar -->
+    <head>
+    <link rel="stylesheet" href="admin/css/estilosHeader.css">
+    </head>
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color:white; border-style:none;">
+    <a class="navbar-brand" href="#"><img src="img/LOGO 2Km.jpg" class="logo-brand" alt="logo"></a>
+    <div class="navbar-header">
+      <div  data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+  </div>
+    </div>
+        <div id="navbar" class="nav navbar-nav navbar-right"">
+          <ul class="nav navbar-nav">
+            
+          <li class="active" ><a href="#"  style="background-color:white; border-style:none; "><i class='glyphicon glyphicon-user'></i> Usuario</a></li>
+           <li class="active"><a href="index.php"  style="background-color:white; border-style:none;"><i class='glyphicon glyphicon-picture'></i> Galería</a></li>
+             <li class="active"><a href="admin/bannerlist.php"  style="background-color:white; border-style:none;"><i class='glyphicon glyphicon-picture'></i> Eliminar y Modificar</a></li>
+              <li class="active"><a href="bannerlist.php"  style="background-color:white; border-style:none;"><i class=' glyphicon glyphicon-remove-circle '></i> Salir</a></li>
+           <li class="active"><a href="bannerlist.php"  style="background-color:white; border-style:none;"> </a></li>
+          
+          </ul>
+        </li>
+      </div>
+    </nav>
+  
